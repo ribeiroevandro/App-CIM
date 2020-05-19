568318801f1ea9a7b63d8b2d856eabae14f0210b
MiriamRodrigues <miriambrodrigue2@gmail.com>
2020-05-18 21:57:41 -0300

    Add mobile project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import Login from './pages/Login';
 import './global.css'
+=======
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <Login />
+=======
+    <h1>Hey</h1>
+>>>>>>> Stashed changes
   );
 }
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 import App from './App';
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
+<<<<<<< Updated upstream
 );
 
 
+=======
+);
+>>>>>>> Stashed changes
