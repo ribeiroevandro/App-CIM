ef04f18a0af995718704411858b3e348f0374e9f
MiriamRodrigues <miriambrodrigues2@gmail.com>
2020-05-20 11:48:27 -0300

    Chamando o drawer no app

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Stack from './src/nav/stack'
+import Drawer from './src/nav/drawer'
 
 export default function App() {
   return (
-    <Stack />
+    <Stack />,
+    <Drawer />
   );
 }
