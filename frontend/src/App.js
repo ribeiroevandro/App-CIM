8d578b177107e397160ba9e11add3b87e21b9b40
MiriamRodrigues <miriambrodrigue2@gmail.com>
2020-07-10 11:34:29 -0300

    ADMIN limpado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Routes from './routes';
-import './global.css'
 
 function App() {
   return (
-    <Routes />
-  );
+      <h1>Hello Wolrd</h1>
+    );
 }
 export default App;
